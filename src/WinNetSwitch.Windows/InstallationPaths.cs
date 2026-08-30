bf501bf6393f2f7111254b7e248a78f0b250685c
wit_qq <belyiwork@mail.ru>
2026-08-30 07:45:57 +0300

    chore: prepare v1.4.1 release

--- src/WinNetSwitch.Windows/InstallationPaths.cs
+++ src/WinNetSwitch.Windows/InstallationPaths.cs
@@ -4,7 +4,7 @@ public static class InstallationPaths
 {
     public const string ApplicationName = "WinNetSwitch";
     public const string ScheduledTaskName = "WinNetSwitch";
-    public const string Version = "1.4.0";
+    public const string Version = "1.4.1";
 
     public static string InstallDirectory { get; } = Path.Combine(
         Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
